2930bc200b06eacd102d9e9b04591886dfa7ca42
Ang <hexiaoang@gmail.com>
2025-07-17 22:39:12 +0800

    Update benchmark.py

--- benchmark.py
+++ benchmark.py
@@ -15,7 +15,7 @@ import uuid, time, statistics
 import ChemInformant as ci
 import pubchempy as pcp
 
-# ---------------- 1. 300 unique drug names ----------------
+# ---------------- 1. 285 unique drug names ----------------
 DRUGS = sorted({
     "abacavir","acetaminophen","acetazolamide","acyclovir","adalimumab",
     "albendazole","albuterol","alclometasone","allopurinol","alprazolam",
